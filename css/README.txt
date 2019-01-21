c99febcbaedb6f9f338a02fa7f1b7f54237a3938
Rick Mesta <rick.mesta@rackspace.com>
2019-01-21 10:13:03 -0600

    Slack update to 3.3.6: Updated instructions
    Signed-off-by: Rick Mesta <rick.mesta@rackspace.com>

--- css/README.txt
+++ css/README.txt
@@ -1,7 +1,7 @@
 #
 # Replace ssb-interop.js file with mine
 #
-sudo cp -p ssb-interop.js /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
+sudo cat ssb-interop-append.js >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
 
 #
 # Place my style sheet in the correct location
---
+++ css/ssb-interop-append.js
@@ -0,0 +1,39 @@
+
+//
+// Dark Slack theme
+//
+// See README.txt
+//
+document.addEventListener("DOMContentLoaded", function() {
+    // Then get its webviews
+    let webviews = document.querySelectorAll(".TeamView webview");
+
+    let customCustomCSS = ``
+
+    let fs = require('fs');
+    for (var file of ['black.css', 'dark.css', 'caiceA.css']) {
+        let filePath = '/Applications/Slack.app/Contents/Resources/' + file;
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err, css) {
+          if (!err) {
+            $("<style></style>").appendTo('head').html(css);
+          }
+        });
+    }
+    // Wait for each webview to load
+    webviews.forEach(webview => {
+        webview.addEventListener('ipc-message', message => {
+            if (message.channel == 'didFinishLoading')
+            // Finally add the CSS into the webview
+                cssPromise.then(css => {
+                let script = `
+let s = document.createElement('style');
+s.type = 'text/css';
+s.id = 'slack-custom-css';
+s.innerHTML = \`${css + customCustomCSS}\`;
+document.head.appendChild(s);
+`
+                webview.executeJavaScript(script);
+            })
+        });
+    });
+});
